cef6814bac4df642ad0bc34847a0d53d63340c94
harith md noor <harithmdnoor@gmail.com>
2020-06-27 21:25:17 +0800

    asd

---
+++ ver2/assets/js/login.js
@@ -0,0 +1,8 @@
+// Initialize the FirebaseUI Widget using Firebase.
+var ui = new firebaseui.auth.AuthUI(firebase.auth());
+ui.start('#firebaseui-auth-container', {
+    signInOptions: [
+      firebase.auth.EmailAuthProvider.PROVIDER_ID
+    ],
+    // Other config options...
+  });
