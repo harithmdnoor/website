f92ab39e7605e567087d2bf9379431ce49391b25
harith md noor <harithmdnoor@gmail.com>
2020-08-22 23:45:16 +0800

    s

--- assets/js/login.js
+++
@@ -1,8 +0,0 @@
-// Initialize the FirebaseUI Widget using Firebase.
-var ui = new firebaseui.auth.AuthUI(firebase.auth());
-ui.start('#firebaseui-auth-container', {
-    signInOptions: [
-      firebase.auth.EmailAuthProvider.PROVIDER_ID
-    ],
-    // Other config options...
-  });
